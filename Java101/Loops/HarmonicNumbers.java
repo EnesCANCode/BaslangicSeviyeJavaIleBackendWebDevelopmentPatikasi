f0de55999e5e94e9322664dad22e95bcfe67d90e
Enes CAN <enesscann98@gmail.com>
2026-04-30 20:29:00 +0300

    HarmonicNumbers added

---
+++ Java101/Loops/HarmonicNumbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class HarmonicNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n;
+        double result = 0.0;
+        System.out.print("Enter the n : ");    n = input.nextInt();
+        for(int i=1; i<=n; i++){
+            result += (1.0 / i);
+        }
+        System.out.println("Result : " + result);
+        input.close();
+    }
+}
