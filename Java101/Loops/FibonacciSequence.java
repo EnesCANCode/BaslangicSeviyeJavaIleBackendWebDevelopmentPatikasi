2fb0fd1796ceafc2e1f83e5c9e7d3bbf70db8b4f
Enes CAN <enesscann98@gmail.com>
2026-04-30 20:58:38 +0300

    FibonacciSequence added

---
+++ Java101/Loops/FibonacciSequence.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class FibonacciSequence {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num, first = 0, second = 1, temp;
+        System.out.print("Enter the number for index of last digit of Fibonacci Sequence : ");    num = input.nextInt();
+        System.out.print(first + " " + second + " ");
+        for(int i=0; i<num-2; i++){
+            temp = first + second;
+            first = second;
+            second = temp;
+            System.out.print(second + " ");
+        }
+        input.close();
+    }
+}
