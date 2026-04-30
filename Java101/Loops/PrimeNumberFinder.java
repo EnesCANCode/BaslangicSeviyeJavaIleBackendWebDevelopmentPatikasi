b1ec065f60852957130e2f48e00ce6a498a33ee3
Enes CAN <enesscann98@gmail.com>
2026-04-30 20:56:43 +0300

    PrimeNumberFinder added

---
+++ Java101/Loops/PrimeNumberFinder.java
@@ -0,0 +1,25 @@
+public class PrimeNumberFinder {
+    public static void main(String[] args) {
+        for (int i = 2; i <=100; i++){
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    private static boolean isPrime(int num){
+        boolean isPrime = true;
+        if (num < 2) {
+            return false;
+        } else{
+            for(int i=2; i<=Math.sqrt(num); i++){
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            return isPrime;
+        }
+    }
+}
